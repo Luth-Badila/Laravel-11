858d56621580f5108c431e81acf245d6a87cc6c0
Luth-Badila <FrancescoLutfi10@gmail.com>
2024-05-26 10:48:16 +0700

    feat: made blog with laravel for the first time

---
+++ resources/views/contact.blade.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Contact</title>
+</head>
+<body>
+    <a href="/">Home</a>
+    <a href="/about">About</a>
+    <a href="/blog">Blog</a>
+    <a href="contact">Contact</a>
+
+    <div>
+        <h1>Contact Page</h1>
+        <ul>
+            <li>
+                My Email : FrancescoLutfi10@gmail.com
+            </li>
+        </ul>
+        <ul>
+            <li>
+                My Instagram : sebuah_kapas
+            </li>
+        </ul>
+    </div>
+</body>
+</html>
