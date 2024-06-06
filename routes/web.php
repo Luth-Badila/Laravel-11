6e083079b2a5c70d70579aaf18fe53711c44af94
Luth-Badila <FrancescoLutfi10@gmail.com>
2024-06-06 20:13:07 +0700

    add: added relation between posts and users

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Models\Post;
+use App\Models\User;
 use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Route;
 
@@ -21,6 +22,13 @@ Route::get('/posts/{post:slug}', function(Post $post) {
     // $post = Post::find($slug);
 
     return view('post', ['title' => 'Single Post', 'post' => $post]);
+});
+
+Route::get('/authors/{user:id}', function(User $user) {
+
+    // $post = Post::find($slug);
+
+    return view('posts', ['title' => 'Articles by ' .$user->name, 'posts' => $user->posts]);
 });
 
 Route::get('/contact', function () {
